7d949e666c412e545ae764d4e15ce82b6b008292
FcoJesusDiaz <FcoJesus.Diaz@alu.uclm.es>
2022-03-13 02:54:41 +0100

    Added more macros

--- include/definitions.h
+++ include/definitions.h
@@ -1,10 +1,17 @@
 #define MAX_LINE 255
 #define NUM_EXAMS 2
 #define MIN_GRADE 5
+#define PIPE_SIZE 2
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+#define PROJECT_NAME "SSOO-p1"
+#define BACKUP_DIR "backup-p1"
 #define LOG_FILENAME "log.txt"
 #define STUDENTS_INFO "estudiantes.txt"
 #define STUDENTS_DIR "estudiantes/"
 #define EXAMS_DIR "examenes/"
-#define EXEC_DIR "exec/"
+#define EXEC_DIR "exec"
+#define OBJ_DIR "obj"
+#define DEBUG_DIR "debug"
 #define MIN_GRADE_FILENAME "nota.txt"
 #define MIN_GRADE_FILE_TEMPLATE "La nota que debes obtener en este nuevo examen para superar la prueba es "
